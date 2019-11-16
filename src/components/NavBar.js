270d594716a06c5dbbf9ceb7bdb1f4164e921487
celenajasmin-tg <rcdjasmin@yahoo.com>
2019-11-16 21:37:16 +0800

    Add active state

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,38 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './NavBar.css'
 
 function NavBar() {
+    const checkActive = (match, location) => {
+        const {pathname} = location;
+        if (pathname === "/"
+            || pathname === "/donor-record"
+            || pathname === "/add-donor") {
+            return true;
+        }
+    }
     return (
         <div className='sidebar bg-light'>
             <div className='sidebar-brand'>
                 <p>United Women</p>
             </div>
             <nav className='sidebar-nav'>
-                <ul className='nav'>
+                <ul>
                     <li className='nav-item'>
-                        <Link to='/' className='nav-link'>Donor Records</Link>
+                        <NavLink to='/'
+                                 className='nav-link'
+                                 isActive={checkActive}
+                                 activeClassName='active'>Donor Records</NavLink>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/donation-records' className='nav-link'>Donation Records</Link>
+                        <NavLink to='/donation-records' className='nav-link' activeClassName='active'>Donation Records</NavLink>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/upload-records' className='nav-link'>Upload Records</Link>
+                        <NavLink to='/upload-records' className='nav-link' activeClassName='active'>Upload Records</NavLink>
                     </li>
                 </ul>
             </nav>
-            <button type="button" className="btn btn-outline-secondary btn-logout">Logout</button>
+            <button type="button" className="btn btn-outline-primary btn-block btn-logout">Logout</button>
         </div>
 
     );
