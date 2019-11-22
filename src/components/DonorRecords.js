ed38ae7da49b3beaaaaf880b69669999c7b4f2c9
Mel Tan <mel.tmxx@gmail.com>
2019-11-22 22:45:49 +0800

    Donor table with pagination logic + exportCSV

--- src/components/DonorRecords.js
+++ src/components/DonorRecords.js
@@ -1,34 +1,37 @@
-import React from "react";
-import "./DonorRecords.css";
-import NavBarWrapper from "../helpers/NavBarWrapper";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom';
+import DropdownFilter from './DropdownFilter';
+import DonorTable from './DonorTable';
+import './DonorRecords.css';
+import NavBarWrapper from '../helpers/NavBarWrapper';
+import ExportCSV from './ExportCSV';
 
 const DonorRecords = () => {
-  const users = [
-    { name: `Amy Lim`, id: 1 },
-    { name: `Kelvin Ng`, id: 2 },
-    { name: `Joyce Lee`, id: 3 }
-  ];
-
   return (
-    <>
-      <h1>Donor Records</h1>
-      {/* for testing */}
-
-      {users.map((user, index) => (
-        <Link
-          key={index}
-          to={`/donors/${user.id}/donations`}
-          className="nav-link"
-        >
-          {user.name}
-        </Link>
-      ))}
-      <Link to="/add-donor" className="nav-link">
-        Add donor
-      </Link>
-    </>
+    <div className="donorContainer offset-md-3">
+      <div className="row justify-content-md-between">
+        <h1 className="col-md-8 font-weight-bold donorHeader">
+          Donor Records
+        </h1>
+          <Link to='/' className="btn btn-outline-primary col-md-2 addDonorBtn" >Add Donor</Link>
+      </div>
+      <div className="row" style={{marginTop:'35px'}}>
+      <div className="col">
+          <DropdownFilter />
+        </div>
+      </div>
+      <div className="row justify-content-md-end" style={{marginTop:'20px'}}>
+        <div className="col-md-2.5">
+          <ExportCSV />
+        </div>
+      </div>
+      <div className="row" style={{marginTop:'20px'}}>
+        <div className="col">
+          <DonorTable />
+        </div>
+      </div>
+    </div>
   );
-};
+}
 
 export default NavBarWrapper(DonorRecords);
