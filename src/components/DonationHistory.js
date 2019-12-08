006ddf09be118b2cf6d7b7bd9216b31a1d933ef3
celenajasmin-tg <rcdjasmin@yahoo.com>
2019-12-08 10:11:46 +0800

    Add fetching of donation history data from backend

--- src/components/DonationHistory.js
+++ src/components/DonationHistory.js
@@ -1,122 +1,157 @@
-import React, { useState, useEffect } from 'react';
-import Table from 'react-bootstrap/Table'
+import React, { useState, useEffect } from "react";
+import Table from "react-bootstrap/Table";
+import moment from "moment";
 
-const DonationHistory = () => {
-    let totalItems = 0
-    let totalPages = 0
-    const [currentPage, setCurrentPage] = useState(1)
-    const [data, setData] = useState([])
-    const [json, setJson] = useState({})
-    const [loading, setLoading] = useState(false)
+const DonationHistory = props => {
+  let perPage = 1;
+  const [totalItems, setTotalItems] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [data, setData] = useState([]);
 
-    const fetchJson = async (url) => {
-      const json = await fetch(url).then(response => response.json())
-      return json
-    }
+  const [loading, setLoading] = useState(false);
 
-    const loadNewPage = async (pageNumber) => {
-      setLoading(true)
-      const response = await fetchJson(`https://reqres.in/api/users?page=${pageNumber}`)
-      setJson(response)
-      setData(response.data)
-      setLoading(false)
-    }
+  const fetchJson = async url => {
+    const json = await fetch(url).then(response => response.json());
+    return json;
+  };
 
-    useEffect(() => {
-      const fetchPosts = async () => {
-        setLoading(true)
-        const response = await fetchJson("https://reqres.in/api/users?page=1")
-        /* data: Array, page: 1, per_page: 6, total: 12, total_pages: 2*/
-        setJson(response)
-        setData(response.data)
-        setLoading(false)
-      }
-      fetchPosts()
-    }, [])
-
-
-    totalItems = json.total
-    totalPages = json.total_pages
-
-    let perPage = totalItems / totalPages
-
-    const paginate = (pageNumber) => {
-      loadNewPage(pageNumber)
-      setCurrentPage(pageNumber)
-    }
-
-    return (
-      <div className="mt-5">
-        { loading ? <p>Loading</p> :
-        <>
-        <DonationHistoryTable users={data} loading={loading} totalItems={totalItems} currentPage={currentPage}/>
-        <Pagination perPage={perPage} totalItems={totalItems} paginate={paginate} currentPage={currentPage}/>
-        </> }
-    </div>
+  const loadNewPage = async pageNumber => {
+    setLoading(true);
+    const response = await fetchJson(
+      `http://localhost:3001/donors/${props.userId}/donations?page=${pageNumber}&perPage=${perPage}`
+      /*sample: /donors/1/donations?page=3&perPage=12*/
     );
-}
+    setTotalItems(2);
+    setData(response.data);
+    setLoading(false);
+  };
 
-const DonationHistoryTable =({ users, loading, totalItems}) => {
-    if(loading) {
-        return <h2>Loading...</h2>
-    }
-    return (
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const response = await fetchJson(
+        `http://localhost:3001/donors/${props.userId}/donations?page=1&perPage=${perPage}`
+      );
+      /* data: Array, page: 1, per_page: 6, total: 12, total_pages: 2*/
+      setTotalItems(2);
+      setData(response.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
+
+  const paginate = pageNumber => {
+    loadNewPage(pageNumber);
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <div className="mt-5">
+      {loading ? (
+        <p>Loading</p>
+      ) : (
         <>
-        <h2>Donation history</h2>
-        <Table striped hover responsive>
-            <thead>
-                <tr>
-                    <th>Date of Donation</th>
-                    <th>Amount</th>
-                    <th>Source</th>
-                </tr>
-            </thead>
-            <tbody>
-            {users.map(user=>(
-                <tr key={user.id}>
-                    <td>{user.id}</td>
-                    <td>{user.first_name}</td>
-                    <td>{user.last_name}</td>
-                </tr>
-            ))}
-            </tbody>
-        </Table>
-        <small className="float-right text-muted">Showing {users.length} out of {totalItems} items</small>
+          <DonationHistoryTable
+            donations={data}
+            loading={loading}
+            totalItems={totalItems}
+          />
+          <Pagination
+            perPage={perPage}
+            totalItems={totalItems}
+            paginate={paginate}
+            currentPage={currentPage}
+          />
         </>
-    )
-}
+      )}
+    </div>
+  );
+};
+
+const DonationHistoryTable = ({ donations, loading, totalItems }) => {
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  const rows = donations.map(donation => (
+    <tr key={donation.id}>
+      <td>{moment(donation.donationDate).format("Do MMMM YYYY")}</td>
+      <td>${donation.amount.slice(0, -2)}</td>
+      <td>{donation.Campaign.type}</td>
+      <td>{donation.Source.name}</td>
+    </tr>
+  ));
+
+  return (
+    <>
+      <h2>Donation history</h2>
+      <Table striped hover responsive>
+        <thead>
+          <tr>
+            <th>Date of Donation</th>
+            <th>Amount</th>
+            <th>Campaign</th>
+            <th>Source</th>
+          </tr>
+        </thead>
+        <tbody>{donations ? rows : <td>Loading</td>}</tbody>
+      </Table>
+      <small className="float-right text-muted">
+        Showing {donations.length} out of {totalItems} items
+      </small>
+    </>
+  );
+};
 
 const Pagination = ({ perPage, totalItems, paginate, currentPage }) => {
-    const pageNumbers = []
-    for(let i = 1; i <= Math.ceil(totalItems / perPage); i++) {
-        pageNumbers.push(i)
-    }
-    return (
-        <nav>
-            <ul className="pagination">
-                <li className={currentPage === 1 ? "page-item disabled" : "page-item"}>
-                    <a class="page-link" href="#" aria-label="Previous" onClick={() => paginate(--currentPage)}>
-                        <span aria-hidden="true">&laquo;</span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                </li>
-                {pageNumbers.map(number => (
-                    <li key={number} className={currentPage === number ? "page-item active" : "page-item"}>
-                        <a onClick={() => paginate(number)}
-                            className="page-link">
-                            {number}
-                        </a>
-                    </li>
-                ))}
-                <li className={currentPage === pageNumbers.length ? "page-item disabled" : "page-item"}>
-                    <a class="page-link" href="#" aria-label="Next" onClick={() => paginate(++currentPage)}>
-                        <span aria-hidden="true">&raquo;</span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    )
-}
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalItems / perPage); i++) {
+    pageNumbers.push(i);
+  }
+  return (
+    <nav>
+      <ul className="pagination">
+        <li className={currentPage === 1 ? "page-item disabled" : "page-item"}>
+          <button
+            class="page-link"
+            aria-label="Previous"
+            onClick={() => paginate(--currentPage)}
+          >
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">Previous</span>
+          </button>
+        </li>
+        {pageNumbers.map(number => (
+          <li
+            key={number}
+            className={
+              currentPage === number ? "page-item active" : "page-item"
+            }
+          >
+            <button onClick={() => paginate(number)} className="page-link">
+              {number}
+            </button>
+          </li>
+        ))}
+        <li
+          className={
+            currentPage === pageNumbers.length
+              ? "page-item disabled"
+              : "page-item"
+          }
+        >
+          <button
+            class="page-link"
+            aria-label="Next"
+            onClick={() => paginate(++currentPage)}
+          >
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default DonationHistory
+export default DonationHistory;
