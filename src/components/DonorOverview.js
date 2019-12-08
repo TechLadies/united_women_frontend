2f6cbcb7dc6dec6031c9c644b1a308537274c632
celenajasmin-tg <rcdjasmin@yahoo.com>
2019-12-08 11:09:24 +0800

    Pull info from backend for edit donor form

--- src/components/DonorOverview.js
+++ src/components/DonorOverview.js
@@ -1,127 +1,129 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import './DonorRecords.css'
-import Form from 'react-bootstrap/Form'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import EditDonorForm from './EditDonorForm'
-import NavBarWrapper from '../helpers/NavBarWrapper'
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import EditDonorForm from "./EditDonorForm";
+import NavBarWrapper from "../helpers/NavBarWrapper";
+import DonationHistory from "./DonationHistory";
 import { withAuthorisedPageHOC } from '../wrappers/withTokenHOC'
-import DonationHistory from './DonationHistory'
 
+const DonorOverview = props => {
+  const id = props.match.params.id;
+  const [donor, setDonor] = useState({});
+  const [editing, setEditing] = useState(false);
+  const [validated, setValidated] = useState(false);
+  const [initialDonor, setInitialDonor] = useState({});
 
-const DonorOverview = () => {
-  const currentDonor = {
-    name: 'Amy Lim',
-    email: 'amylim@gmail.com',
-    nric: 'S1239879D',
-    type: 'Recurring',
-    salutation: 'Miss',
-    phone: '91234567',
-    address: '123 Tampines West #11-01 Singapore 234567'
-  }
-  const [ donor, setDonor ] = useState(currentDonor)
-  const [ updatedDonor, setUpdatedDonor ] = useState(donor)
-  const [ editing, setEditing ] = useState(false)
-
-  const getUpdatedDonor = donorData => {
-    setUpdatedDonor(donorData)
-  }
+  useEffect(() => {
+    const loadDonorInfo = async () => {
+      const json = await fetch(
+        `http://localhost:3001/donors/${id}`
+      ).then(response => response.json());
+      let initial = {
+        name: json.name,
+        email: json.email,
+        identifier: json.identifier,
+        frequency: json.donorFrequency.donorFrequency,
+        salutation: json.salutation.salutation,
+        contactNo: json.contactNo,
+        address: json.address
+      };
+      setInitialDonor({ ...initial });
+      setDonor({ ...initial });
+    };
+    loadDonorInfo();
+  }, []);
 
   const updateDonor = () => {
-    setEditing(false)
-    setDonor(updatedDonor)
-  }
+    setDonor({
+      name: donor.name,
+      email: donor.email,
+      identifier: donor.identifier,
+      frequency: donor.frequency,
+      salutation: donor.salutation,
+      contactNo: donor.contactNo,
+      address: donor.address
+    });
+  };
+
+  const handleSubmit = event => {
+    const form = event.currentTarget.form;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      setValidated(true);
+    } else {
+      setEditing(false);
+      setValidated(false);
+      updateDonor();
+    }
+  };
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setDonor({ ...donor, [name]: value });
+  };
+
   const SaveButton = () => (
-    <button onClick={updateDonor} className="btn btn-primary">Save edits</button>
-  )
+    <button
+      onClick={handleSubmit}
+      className="btn btn-primary"
+      disabled={!editing}
+      form="donor-form"
+      type="submit"
+    >
+      Save edits
+    </button>
+  );
 
   const CancelButton = () => (
-    <button onClick={()=>setEditing(false)} className="btn btn-link">Cancel</button>
-  )
+    <button
+      onClick={() => {
+        setDonor({});
+        setEditing(false);
+      }}
+      className="btn btn-link"
+    >
+      Cancel
+    </button>
+  );
 
   const EditButton = () => (
-    <button onClick={()=>setEditing(true)} className="btn btn-outline-primary">Edit donor</button>
-  )
-
-  /*render*/
-  return(
-      <>
-      <h1>Donor Records</h1>
-      <div className="breadcrumbs">
-        <p><Link to="/">Donor List</Link>  >  <strong>{donor.name}</strong></p>
-        { editing ? <div className="btn-toolbar"><CancelButton/><SaveButton/></div> : <EditButton/> }
-      </div>
-
-      { editing ? (
-        <div>
-        <EditDonorForm currentDonor={donor} getUpdatedDonor={getUpdatedDonor}/>
-        </div>
-      ) : (
-        <div>
-        <DonorDetails currentDonor={donor}/>
-        </div>
-      )}
-      <DonationHistory/>
-    </>
-  )
-}
-
-
-
-const DonorDetails = props => {
+    <button
+      onClick={() => setEditing(true)}
+      className="btn btn-outline-primary"
+    >
+      Edit donor
+    </button>
+  );
 
   return (
-    <Form>
-      <Row className="donor-summary__container">
-        <Col md={6} className="donor-summary__column">
-          <p className="donor-summary__title">Identity Details</p>
-          <Row>
-            <Form.Group as={Col} controlId="donorNRIC" column sm="6">
-              <Form.Label>NRIC</Form.Label>
-              <Form.Control plaintext readOnly defaultValue={props.currentDonor.nric} />
-            </Form.Group>
-            <Form.Group as={Col} controlId="donorType" column sm="6">
-              <Form.Label>Type</Form.Label>
-              <Form.Control plaintext readOnly defaultValue={props.currentDonor.type} />
-            </Form.Group>
-          </Row>
-          <Row>
-            <Form.Group as={Col} controlId="donorSalutation" column sm="6">
-              <Form.Label>Salutation</Form.Label>
-              <Form.Control plaintext readOnly defaultValue={props.currentDonor.salutation} />
-            </Form.Group>
-            <Form.Group as={Col} controlId="donorName" column sm="6">
-              <Form.Label>Name</Form.Label>
-              <Form.Control plaintext readOnly defaultValue={props.currentDonor.name} />
-            </Form.Group>
-          </Row>
-        </Col>
-        <Col md={6} className="donor-summary__column">
-        <p className="donor-summary__title">Contact Information</p>
-          <Row>
-            <Form.Group as={Col} controlId="donorEmail" column sm="6">
-              <Form.Label>Email</Form.Label>
-              <Form.Control plaintext readOnly type="email" defaultValue={props.currentDonor.email} />
-            </Form.Group>
-            <Form.Group as={Col} controlId="donorPhone" column sm="6">
-              <Form.Label>Phone</Form.Label>
-              <Form.Control plaintext readOnly defaultValue={props.currentDonor.phone} />
-            </Form.Group>
-          </Row>
-          <Row>
-            <Form.Group as={Col} controlId="donorAddress" column sm="12">
-              <Form.Label>Address</Form.Label>
-              <Form.Control plaintext readOnly defaultValue={props.currentDonor.address} />
-            </Form.Group>
-          </Row>
-          </Col>
-      </Row>
-    </Form>
-  )
-}
+    <>
+      <h1>Donor Records</h1>
 
-
-
+      <div className="breadcrumbs">
+        <p>
+          <Link to="/">Donor List</Link> > <strong>{donor.name}</strong>
+        </p>
+        {editing ? (
+          <div className="btn-toolbar">
+            <CancelButton />
+            <SaveButton />
+          </div>
+        ) : (
+          <EditButton />
+        )}
+      </div>
+      <div>
+        <EditDonorForm
+          donor={donor}
+          editing={editing}
+          handleInputChange={handleInputChange}
+          validated={validated}
+          handleSubmit={handleSubmit}
+        />
+      </div>
+      <DonationHistory props={props} userId={id} />
+    </>
+  );
+};
 
 export default withAuthorisedPageHOC(NavBarWrapper(DonorOverview))
--- src/components/DonorRecords.js
+++ src/components/DonorRecords.js
@@ -1,17 +1,34 @@
-import React from 'react'
-import './DonorRecords.css'
-import NavBarWrapper from '../helpers/NavBarWrapper'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./DonorRecords.css";
+import NavBarWrapper from "../helpers/NavBarWrapper";
+import { Link } from "react-router-dom";
 
 const DonorRecords = () => {
-  return(
-      <>
-        <h1>Donor Records</h1>
-        {/* for testing */}
-        <Link to='/donor-record' className='nav-link'>Amy Lim</Link>
-        <Link to='/add-donor' className='nav-link'>Add donor</Link>
-      </>
-  )
-}
+  const users = [
+    { name: `Amy Lim`, id: 1 },
+    { name: `Kelvin Ng`, id: 2 },
+    { name: `Joyce Lee`, id: 3 }
+  ];
 
-export default NavBarWrapper(DonorRecords)
+  return (
+    <>
+      <h1>Donor Records</h1>
+      {/* for testing */}
+
+      {users.map((user, index) => (
+        <Link
+          key={index}
+          to={`/donors/${user.id}/donations`}
+          className="nav-link"
+        >
+          {user.name}
+        </Link>
+      ))}
+      <Link to="/add-donor" className="nav-link">
+        Add donor
+      </Link>
+    </>
+  );
+};
+
+export default NavBarWrapper(DonorRecords);
